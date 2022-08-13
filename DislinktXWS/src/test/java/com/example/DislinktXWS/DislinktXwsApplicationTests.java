f247cbb7968fd9b018f1b17deb7c0a77454dbe7b
trpka <64914510+trpka@users.noreply.github.com>
2022-08-14 00:01:45 +0200

    added initial backend

---
+++ DislinktXWS/src/test/java/com/example/DislinktXWS/DislinktXwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DislinktXWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DislinktXwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
